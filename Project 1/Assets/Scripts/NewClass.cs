0d4a01aa16c6b8c943eaf14b0486536e1982f331
Jacob Hansen <khanjake@gmail.com>
2020-01-15 10:50:36 -0700

    Unity Day 2
    Learned more parts of C#
    Variable lecture

--- Project 1/Assets/Scripts/NewClass.cs
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// Don't remove {} they define the body of the class
 public class NewClass : MonoBehaviour
 {
+    // public is to say its open, string is a data type, give it a name
+    // Always end single lines with a ;
+    // Always have public or private. Private is hidden and default
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World");
+        print(nameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
+        // Variable Assigned in Unity
     }
 
     // Update is called once per frame
