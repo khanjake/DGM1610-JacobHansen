537e77c0fcc2600823e3d2f16c8b366cb145901c
Jacob Hansen <khanjake@gmail.com>
2020-01-22 11:22:26 -0700

    Events
    Created events

--- Project 1/Assets/Scripts/NewClass.cs
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -1,29 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
+
 // Don't remove {} they define the body of the class
 public class NewClass : MonoBehaviour
 {
-    // public is to say its open, string is a data type, give it a name
-    // Always end single lines with a ;
-    // Always have public or private. Private is hidden and default
     public string nameString;
     public int intNum;
     public float floatNum;
     public GameObject gameObj;
-    // Start is called before the first frame update
+    public UnityEvent uEvent;
     void Start()
     {
         print(nameString);
         print(intNum);
         print(floatNum);
         print(gameObj);
-        // Variable Assigned in Unity
     }
-
-    // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
