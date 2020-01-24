f23a75cb33537b58619f9d9d734032184da3f893
Jacob Hansen <khanjake@gmail.com>
2020-01-24 11:34:59 -0700

    Daily Coding
    25 lines

---
+++ Project 1/Assets/Scripts/NewScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewScript : MonoBehaviour
+{
+    public UnityEvent testEvent;
+    private int hiddenInt;
+    public UnityEvent testEventTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testEvent.Invoke();
+        testEventTwo.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(hiddenInt);
+    }
+}
