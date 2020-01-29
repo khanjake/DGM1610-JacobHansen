0dd5a3a83b7ba8d340aa5423c98333b79ed34eb7
Jacob Hansen <khanjake@gmail.com>
2020-01-29 11:20:01 -0700

    Review
    Created more events and ways to invoke them

---
+++ Project 1/Assets/Scripts/AnythingYouWant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnythingYouWant : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseUpEvent, mouseDragEvent;
+    public UnityEvent mouseEnterEvent, mouseExitEvent, mouseOverEvent, mouseUpAsButtonEvent;
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        mouseUpAsButtonEvent.Invoke();
+    }
+}
---
+++ Project 1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public int intTriggeringEvents;
+    public float floatTriggeringEvents;
+    public string stringTriggeringEvents;
+    public UnityEvent triggeringEventsEnter;
+    public UnityEvent triggeringEventsStay;
+    public UnityEvent triggeringEventsExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+        triggeringEventsEnter.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        triggeringEventsStay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggeringEventsExit.Invoke();
+    }
+}
