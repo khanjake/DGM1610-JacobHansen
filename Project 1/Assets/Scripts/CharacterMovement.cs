fca4319c333fc33bb9a45e2776df6c6ebc11960b
Jacob Hansen <khanjake@gmail.com>
2020-02-03 11:21:05 -0700

    Character Controller
    Another Character controller with movement and gravity assigned as a value.

---
+++ Project 1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float characterSpeedMultipler = 5f;
+    private float gravity = -1.5f;
+    public float jumpForce = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*characterSpeedMultipler;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
--- Project 1/Assets/Scripts/NewBehaviourScript.cs
+++ Project 1/Assets/Scripts/NewBehaviourScript.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-public class NewScript : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public UnityEvent testEvent;
-    private int hiddenInt;
-    public UnityEvent testEventTwo;
     // Start is called before the first frame update
     void Start()
     {
-        testEvent.Invoke();
-        testEventTwo.Invoke();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
