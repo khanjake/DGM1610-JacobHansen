2ddfe70fd5ee1a9cdcf704b7fa4e01a63408822e
Jacob Hansen <khanjake@gmail.com>
2020-02-24 11:14:49 -0700

    Added More Bars
    Few lines of code for a lot of work and output

--- Project 1/Assets/Scripts/ImageController.cs
+++ Project 1/Assets/Scripts/ImageController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ImageController : MonoBehaviour
 {
     private Image imageObj;
+    public FloatData dataObj;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class ImageController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        imageObj.fillAmount = dataObj.value;
     }
 }
---
+++ Project 1/Assets/Scripts/TextBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class TextBehavior : MonoBehaviour
+{
+    private Text textObj;
+    public IntData dataObj;
+    void Start()
+    {
+        textObj = getComponent<text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textObj.text = dataObj.value.ToString();
+    }
+}
