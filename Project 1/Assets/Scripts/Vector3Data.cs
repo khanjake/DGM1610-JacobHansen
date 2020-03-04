35deaac34df3a6e6f24990ed059bcc198b9c84c5
Yoga Boy <10857507@my.uvu.edu>
2020-03-04 10:47:24 -0700

    Laptop Down
    Gaming laptop not responding. Going to diagnose later tonight if I have time

---
+++ Project 1/Assets/Scripts/Vector3Data.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 value;
+	
+	public void ChangePosition(Transform transformObj)
+	{
+		transformObj.position = value;
+	}
+}
